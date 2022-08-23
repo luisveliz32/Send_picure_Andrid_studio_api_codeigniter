c975589ed7a96932e93e23b9b48ef6eb11c2b452
luis veliz <luisveliz32@gmail.com>
2022-08-23 11:00:59 -0400

    commit inicial(initial commit)

---
+++ app/src/main/java/com/example/send_picure_andrid_studio_api_codeigniter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.send_picure_andrid_studio_api_codeigniter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
